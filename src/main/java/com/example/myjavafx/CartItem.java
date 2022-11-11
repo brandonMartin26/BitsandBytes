f6afec300deba4bafaaa3286bf4a8d6484c600f9
Danmi Huynh <nhuynh7@asu.edu>
2022-11-11 05:00:19 -0700

    Added CartItem with functions to modify cart

---
+++ src/main/java/com/example/myjavafx/CartItem.java
@@ -0,0 +1,43 @@
+package com.example.myjavafx;
+
+public class CartItem {
+    private Item item;
+    private int count;
+    public CartItem(Item item) {
+        this.item = item;
+        this.count = 1;
+    }
+    public CartItem(Item item, int count) {
+        this.item = item;
+        this.count = count;
+    }
+
+    public void addOne() {
+        this.count++;
+    }
+
+    public void removeOne() {
+        this.count--;
+    }
+
+    public Item getItem() {
+        return this.item;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
+    }
+
+    public int getAmount() {
+        return this.count;
+    }
+
+    public void setAmount(int count) {
+        this.count = count;
+    }
+
+    public double getPrice() {
+        return item.getPrice() * this.count;
+    }
+
+}
