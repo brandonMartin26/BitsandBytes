dbb9c946642eae1bebb6ca0660d42e6e9b23641a
EthanHubbard <ethan.c.hubbard@gmail.com>
2022-11-14 15:33:33 -0700

    process agent changed to listview

--- src/main/java/com/example/myjavafx/A6_ProcessAgentController.java
+++ src/main/java/com/example/myjavafx/A6_ProcessAgentController.java
@@ -1,32 +1,36 @@
 package com.example.myjavafx;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.ResourceBundle;
 
-public class A6_ProcessAgentController {
-    //FXML Variables
-    @FXML private Button processLoadOrdersBtn, acceptOrderButton, chefBtnPA,orderUpdateBtnPA;
-    @FXML private Label orderToProcessLabel;
-    @FXML private TextArea processingTextArea;
-    //Scene Variables
+public class A6_ProcessAgentController implements Initializable {
+    @FXML
+    private ListView processAgentListView;
+
+    @FXML
+    private Button acceptOrderButton;
+
     private Stage stage;
     private Scene scene;
     private Parent root;
-
-    //Order Variables
-    static Order newOrder = new Order();
-    ArrayList<Order> orderList = OrderAPI.getOrderInfo();
+    private Order selectedOrder;
+    static ArrayList<Order> orderList = OrderAPI.getOrderInfo();
+    ObservableList<Order> orders = FXCollections.observableArrayList();
+    ObservableList<String> names = FXCollections.observableArrayList();
 
     public void switchToChefView(ActionEvent event) throws IOException
     {
@@ -37,20 +41,31 @@ public class A6_ProcessAgentController {
         stage.show();
     }
     public void switchToProcessing(ActionEvent event) throws IOException {
-        root = FXMLLoader.load(getClass().getResource("b5_ProcessUpdateView.fxml"));
-        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+        this.root = (Parent)FXMLLoader.load(this.getClass().getResource("processingPage.fxml"));
+        this.stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        this.scene = new Scene(this.root);
+        this.stage.setScene(this.scene);
+        this.stage.show();
+    }
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        names.clear();
+        for(Order order : orderList) {
+            orders.add(order);
+            names.add(order.toString());
+        }
+        processAgentListView.setItems(names);
+        processAgentListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
     }
     public void processingAgentAccept(ActionEvent event) throws IOException {
-        orderList.get(0).status = 1;
-        processingTextArea.clear();
+        selectedOrder = (Order)processAgentListView.getSelectionModel().getSelectedItem();
+        selectedOrder.status = 1;
+        names.remove(processAgentListView.getSelectionModel().getSelectedIndex());
     }
     public void processingViewOrder(ActionEvent event) throws IOException {
-        processingTextArea.clear();
+        //processingTextArea.clear();
         Boolean moreOrders = true;
-        String tempOrder = newOrder.toString();
+        /*String tempOrder = newOrder.toString();
         while(moreOrders) {
             Label orderLabel = new Label();
             int iend = tempOrder.indexOf(";");
@@ -70,6 +85,6 @@ public class A6_ProcessAgentController {
                 tempOrder = tempOrder.replace(tempOrder.substring(0,iend3+1), "");
             }
             processingTextArea.setText("" + pizzaType + "\n\t" + toppings + "\n");
-        }
+        }*/
     }
 }
