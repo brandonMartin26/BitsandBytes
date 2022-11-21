79e344c9bf66cba484fadbe477adbdff09d059aa
Danmi Huynh <nhuynh7@asu.edu>
2022-11-21 15:47:56 -0700

    Update timer

--- src/main/java/com/example/myjavafx/A5_ProcessUpdateController.java
+++ src/main/java/com/example/myjavafx/A5_ProcessUpdateController.java
@@ -16,10 +16,7 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-import java.util.ResourceBundle;
+import java.util.*;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -31,8 +28,10 @@ import com.example.myjavafx.core.models.enums.OrderStatus;
 
 public class A5_ProcessUpdateController implements Initializable {
 
-    @FXML private Label yourOrderIsLabel, acceptedLabel, readyToCookLabel, cookingLabel, readyLabel;
+    @FXML private Label yourOrderIsLabel, acceptedLabel, readyToCookLabel, cookingLabel, readyLabel, timerLabel;
     @FXML private Button refreshOrder, chefViewTempBtn, processAgentOrderUpdateBtn;
+    int counter, seconds, minutes;
+    String lastChecked = "";
 
     private Stage stage;
     private Scene scene;
@@ -51,6 +50,7 @@ public class A5_ProcessUpdateController implements Initializable {
         Platform.runLater(() -> {
             startUpdater();
         });
+        startTimer();
     }
 
     private void stopUpdater() {
@@ -86,5 +86,53 @@ public class A5_ProcessUpdateController implements Initializable {
             }
         };
         executor.scheduleAtFixedRate(loop, 0, 1000, TimeUnit.MILLISECONDS);
+    }
+
+    public void startTimer(){
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask(){
+            @Override
+            public void run() {
+                try {
+                    api = new OrderApiImpl();
+                    String check = OrderRecord.orderStatusToString(api.getOrderStatus(checkoutOrder.orderId));
+
+                    Platform.runLater(() -> {
+                        seconds=counter%60;
+                        minutes=counter/60;
+                        if (counter == 0 && check.equals(OrderRecord.orderStatusToString(OrderStatus.READYTOCOOK))) {
+                            counter = 30 * 60;
+                        } else if (counter == 0 && check.equals(OrderRecord.orderStatusToString(OrderStatus.ACCEPTED))) {
+                            counter = 15 * 60;
+                        } else if(check.equals(OrderRecord.orderStatusToString(OrderStatus.COOKING)) && !check.equals(lastChecked)){
+                            counter = 15 * 60;
+                        } else if(check.equals(OrderRecord.orderStatusToString(OrderStatus.READY))){
+                            counter = 0;
+                        }
+                        lastChecked = check;
+
+                        if(check.equals(OrderRecord.orderStatusToString(OrderStatus.READY))){
+                            timerLabel.setText("Your order is ready!");
+                        }
+                        else if (seconds < 10 && minutes < 10) {
+                            timerLabel.setText("0" + minutes + ":0" + seconds);
+                        } else if (minutes < 10) {
+                            timerLabel.setText("0" + minutes + ":" + seconds);
+                        } else if (seconds < 10) {
+                            timerLabel.setText(minutes + ":" + "0" + seconds);
+                        } else {
+                            timerLabel.setText(minutes + ":" + seconds);
+                        }
+
+                        counter--;
+                        if (counter < 0){
+                            counter = 0;
+                        }
+                    });
+                } catch(Exception e){
+                    throw new RuntimeException(e);
+                }
+            };
+        }, 1000, 1000);
     }
 }
